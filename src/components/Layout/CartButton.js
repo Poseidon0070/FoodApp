d5978abf6dccac76a2225390e0df3fe3edc16942
Shikhar <criticalxsynergy@gmail.com>
2023-10-09 01:27:05 +0530

    v-3

--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,12 +1,31 @@
+import {useState} from "react"
 import CartIcon from "../Cart/CartIcon"
 import style from "./CartButton.module.css"
+import Modal from "../UI/Modal"
 let CartButton = () => {
+    let [open,setModal] = useState(false)
     return (
-        <button className={style['Cartbtn']}>
-            <span><CartIcon></CartIcon></span>
-            <span className="fw-bold">&nbsp;&nbsp;Your Cart&nbsp;&nbsp;</span>
-            <span className={style['count']}>3</span>
-        </button>
+        <>
+            <button className={style['Cartbtn']} onClick={() => setModal(true)}>
+                <span><CartIcon></CartIcon></span>
+                <span className="fw-bold">&nbsp;&nbsp;Your Cart&nbsp;&nbsp;</span>
+                <span className={style['count']}>3</span>
+            </button>
+            <Modal open={open} onClose={() => setModal(false)}>
+                <div>
+                    <h4>Sushi</h4>
+                    <div className="d-flex justify-content-between">
+                        <h2 className="fw-bold">Total Amount</h2>
+                        <h2 className="fw-bold">35.00</h2>
+                    </div>
+                    <div className="d-flex flex-row-reverse">
+                        <input type="button" className="btn btn-lg btn-warning rounded-5" value=" Order "></input>
+                        <input type="button" className="btn btn-lg btn-outline-warning rounded-5 me-4" value=" Close " onClick={() => setModal(false)}></input>
+                    </div>
+                </div>
+            </Modal>
+        </>
+        
     )
 }
 
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,40 @@
+import  ReactDOM  from "react-dom";
+let MODAL_STYLE = {
+    position: "fixed",
+    left: "50%",
+    top: "30%",
+    transform: "translate(-50%,-50%)",
+    backgroundColor: "#FFF",
+    padding: "25px",
+    zIndex: "1000",
+    width:"45vw",
+    fontFamily:'Varela Round',
+    boxShadow: "0px 3px 4px 5px rgb(36, 34, 34)",
+    borderRadius: "10px"
+}
+let OVERLAY_STYLE = {
+    position: "fixed",
+    top:0,
+    left:0,
+    bottom:0,
+    right:0,
+    backgroundColor: "rgba(0,0,0,0.7)",
+    zIndex: "900"
+}
+let Modal = ({open,children,onClose}) => {
+    if(!open) return null;
+    return ReactDOM.createPortal(
+        <>
+            <div style={OVERLAY_STYLE} />
+            <div style={MODAL_STYLE}>
+                <div className="d-flex">
+                    <button className="btn btn-lg btn-close ms-auto" onClick={onClose}></button>
+                </div> 
+                {children}
+            </div>
+        </>,
+        document.getElementById("portal")
+    )
+}
+
+export default Modal;
